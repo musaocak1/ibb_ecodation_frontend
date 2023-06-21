1e91f5a933b267342633ef02074b81beac5df80d
musaocak1 <musaocak1@gmail.com>
2023-06-21 19:05:23 +0300

    4th day ended

--- template.js
+++ template.js
@@ -1,1 +1,27 @@
-// alert("deneme")
+//Dark Mode
+let darkModu = () => {
+    //alert("Dark Modu");
+    document.body.classList.toggle("dark_mode");
+    
+
+}
+
+//Input search
+$(document).ready(function () {
+    const searchApi = ["Adana", "Balikesir", "Çorum", "Denizli", "Diyarbakir", "Edirne", "Malatya", "Van"];
+    $("#tags").autocomplete({
+        source: searchApi
+    })
+});//end
+
+
+// Footer
+let newDate = () => {
+    const date = new Date().getUTCFullYear();
+    //JS DOM
+    //document.getElementById("date_id").innerHTML - new Date().getFullYear();
+
+    //Jquery DOM
+    $("#date_id").html(date);
+}
+newDate()
