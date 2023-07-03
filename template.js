61c296b365edba02360ef362a7b645103ef72b20
musaocak1 <musaocak1@gmail.com>
2023-07-03 17:21:07 +0300

    July 19th-Register

--- template.js
+++ template.js
@@ -2,7 +2,7 @@
 let darkModu = () => {
     //alert("Dark Modu");
     document.body.classList.toggle("dark_mode");
-    
+
 
 }
 
@@ -25,3 +25,50 @@ let newDate = () => {
     $("#date_id").html(date);
 }
 newDate()
+
+const register = document.getElementById("register")
+const closebtn = document.getElementById("closeBtn")
+const form = document.getElementById("formValue")
+register.addEventListener("click", () => {
+    document.querySelector(".register-show").classList.add("show")
+})
+
+
+closebtn.addEventListener("click", () => {
+    document.querySelector(".register-show").classList.remove("show")
+})
+
+let user = []
+
+// Register 
+const clear = () => {
+    document.getElementById("firstName").value = ""
+    document.getElementById("lastName").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("check").value = ""
+    document.getElementById("password").value = "";
+}
+
+form.addEventListener("submit", (e) => {
+
+    const firstName = document.getElementById("firstName").value
+    const lastName = document.getElementById("lastName").value
+    const email = document.getElementById("email").value
+    const check = document.getElementById("check").value
+    const password = document.getElementById("password").value
+
+    user.push({
+        "firstName": firstName,
+        "lastname": lastName,
+        "email": email,
+        "check": check,
+        "password": password
+    })
+    document.querySelector(".register-show").classList.remove("show")
+
+    clear()
+
+    e.preventDefault()
+
+    console.log(user)
+})
